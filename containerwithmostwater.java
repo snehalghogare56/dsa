1f178ef65466a774711a5f5bdab6e276958032d6
snehalghogare56 <118335168+snehalghogare56@users.noreply.github.com>
2024-06-14 19:35:47 +0530

    Update containerwithmostwater.java

--- containerwithmostwater.java
+++ containerwithmostwater.java
@@ -1,20 +1,19 @@
 class Solution {
     public int maxArea(int[] height) {
-        int maxArea=0;
-        int lp=0;
-        int rp=height.length-1;
-        while(lp < rp){
-            int ht= Math.min(height[lp], height[rp]);
-            int wd=rp-lp;
-            int water= ht * wd;
-            maxArea= Math.max(maxArea, water);
-            if(lp <rp){
+        int maxArea = 0;
+        int lp = 0;
+        int rp = height.length - 1;
+        while (lp < rp) {
+            int ht = Math.min(height[lp], height[rp]);
+            int wd = rp - lp;
+            int water = ht * wd;
+            maxArea = Math.max(maxArea, water);
+            if (height[lp] < height[rp]) {
                 lp++;
-            }else{
-                rp++;
+            } else {
+                rp--;
             }
         }
-
         return maxArea;
     }
 }
